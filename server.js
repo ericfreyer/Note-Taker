1976b4c5387459a1a5a576b8d82b7ed8adf4770d
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-01-31 10:54:25 -0500

    got index and notes html pages connected to server

--- server.js
+++ server.js
@@ -18,11 +18,20 @@ function handleRequest(request, response) {
     response.end("It Works!! Path Hit: " + request.url);
   }
 
+//get requests to connect index and notes html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
 
 //setting up simple listen function to see if server is working.
-const PORT = 3000
-var server = http.createServer(handleRequest);
-server.listen(PORT, function() {
+const PORT = 8080
+// var server = http.createServer(handleRequest);
+app.listen(PORT, function() {
 
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
