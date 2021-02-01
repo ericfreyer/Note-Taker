6dd16d5fe1accb1e2a785eb1cc127f3fbae61dbd
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-02-01 13:27:42 -0500

    added module 'uuidv4' to give not unique id number

--- server.js
+++ server.js
@@ -2,9 +2,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const PORT = 8080
-
-
 
 //setting up express
 var app = express();
@@ -12,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-//HTML ROUTES
+//HTML ROUTES================================================================
 
 
 app.get("/", (require, response) => {
@@ -23,25 +22,21 @@ app.get("/notes", (require, response) => {
     response.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-// app.get('*', function(request, response) {
-//     response.sendFile(path.join(__dirname, '/public/index.html'));
-//   });
-
-//API ROUTES
+//API ROUTES==================================================================
 
 /*
 -app.get (`/api/notes`) 
     [X]-read the `db.json` file and return all saved notes as JSON
 */
+//============================================================================
 
-//--------------------------------------------------------------------------
+
 app.get("/api/notes", (require, response) => {
     response.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
 
-//---------------------------------------------------------------------------
-
+//============================================================================
 /*
 -app.post (`/api/notes`) 
     [X]- read function
@@ -50,9 +45,7 @@ app.get("/api/notes", (require, response) => {
     [X]-PUSH note to the `db.json` file, and then return the new note to the client.
     []-give each note unique ID when saved
 */
-
-//--------------------------------------------------------------------------
-//read and write functions for post request.
+//============================================================================
 const readNote = () => {
     const noteText = JSON.parse
         (fs.readFileSync
@@ -72,14 +65,15 @@ const writeNote = function (noteText) {
 app.post("/api/notes", (require, response) => {
     let newNote = require.body;
     let noteArry = readNote();
+    newNote.id = uuidv4();
+    console.log(uuidv4());
     noteArry.push(newNote);
         writeNote(noteArry);
         return response.json(noteArry)
 });
 
 
-//---------------------------------------------------------------------------
-
+//============================================================================
 
 /*
 -app.delete (`/api/notes/:id`) receive a query parameter containing the id of a note to delete.
@@ -88,13 +82,13 @@ app.post("/api/notes", (require, response) => {
     -rewrite the notes to the `db.json` file
 */
 
-//--------------------------------------------------------------------------
+//============================================================================
 app.delete("/api/notes/:id", (require,response) => {
-
+    let noteArry = readNote();
 });
 
 
-//---------------------------------------------------------------------------
+//============================================================================
 
 
 
